1edd2221953a9f0bd587d8572d0e66faa8b5af04
vinay1500 <vinaygautam1500@gmail.com>
2025-11-06 14:23:11 +0530

    Valid Parenthesis problem solved

--- practice leetcode/Stack/Valid_Parentheses.py
+++ practice leetcode/Stack/Valid_Parentheses.py
@@ -1,3 +1,22 @@
 class Solution:
     def isValid(self, s: str) -> bool:
        stack = []
+       brackets = { ")":"(","}":"{","]":"["}
+       #counter = 0
+
+       for c in s:
+           print(stack)
+           if c in brackets:
+            if stack and stack[-1] == brackets[c]:
+               stack.pop()
+               #counter -= 1
+            else:
+               return False
+           else:
+               stack.append(c)
+               #counter += 1
+       return True if not stack else False
+
+s = "()"
+myObj = Solution()
+print(myObj.isValid(s))

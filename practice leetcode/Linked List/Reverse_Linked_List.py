3c4d5d6f21cbc27be96dd4ec54defe0099b2826e
vinay1500 <vinaygautam1500@gmail.com>
2025-11-19 15:11:23 +0530

    some slight changes in functions

--- practice leetcode/Linked List/Reverse_Linked_List.py
+++ practice leetcode/Linked List/Reverse_Linked_List.py
@@ -4,14 +4,14 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
-        new_Head = head
+    def reverseList(self, head):
+            if not head:
+                 return None
+            new_Head = head
 
-        if head.next:
-            new_Head = self.reverseList(head.next)
-            head.next.next = head
-        head.next = None
-
-        return new_Head
+            if head.next:
+                 new_Head = self.reverseList(head.next)
+                 head.next.next = head
+            head.next = None
+            
+            return new_Head
